9f0cab02661c75b7be73318f828e00eb358c0d9e
Юлия <>
2022-09-13 13:48:06 +0300

    25

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+
+while (b > 0)
+{
+   Console.WriteLine($" {Math.Pow(a, b)} ");
+}
+
